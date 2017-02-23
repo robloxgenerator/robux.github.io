e83a9942bebac17f4ef2353e843d8744760e8a5e
mwear3robloxandroid <mitchellwears@gmail.com>
2017-02-23 02:04:58 +0000

    Create cheat.js

---
+++ cheat.js
@@ -0,0 +1,7 @@
+get.&lement.robux($(type="www.roblox.com")
+add.player.robux($(type="robux")
+value = "100000000"
+function="roblox"
+get.Inspect.&lement($(type="edithtml")
+
+find.player.robux($(type="robux")
