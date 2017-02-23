b22e1735d37d4d795a0dd784b8a784ae27ea79f6
mwear3robloxandroid <mitchellwears@gmail.com>
2017-02-23 01:52:32 +0000

    Create robux.js

---
+++ robux.js
@@ -0,0 +1,7 @@
+get.&lement.robux($(type="robux")
+get.generate.robux($(type="robux")
+get.player.robux($(type="robux")
+
+function="robux")
+get.player.robux = Value add.player.robux
+get.player.robux add = 10000000
